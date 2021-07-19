156dc3c012f015cd064003e75fc5ba6346535494
mr.kunal1995@gmail.com <mr.kunal1995@gmail.com>
2021-07-19 20:49:03 +0530

    configure Heroku build

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/examportal-frontend'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/examportal-frontend/'}),
-);
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const ngApp = express();
+ngApp.use(express.static('./dist/examportal-frontend'));
+ngApp.get('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, '/dist/examportal-frontend/index.html'));
+});
+ngApp.listen(process.env.PORT || 8080);
